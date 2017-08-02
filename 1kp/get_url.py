ddb240df8d06107c32bad6676c832d80928cd3db
Workstation <wpwupingwp@outlook.com>
2017-08-02 17:03:39 +0800

    add main()

--- 1kp/get_url.py
+++ 1kp/get_url.py
@@ -4,28 +4,39 @@ import os
 from urllib import request
 
 
-def down():
-    URL = r'http://www.onekp.com/public_data.html'
-
-    raw = request.urlopen(URL)
+def down(url, html):
+    raw = request.urlopen(url)
     if raw.code != '200':
-        raise Exception('Cannot open {}. Quit now.'.format(URL)
-    with open('public_data.html', 'w') as output:
+        raise Exception('Cannot open {}. Quit now.'.format(url))
+    with open(html, 'w') as output:
         output.write(raw.read())
 
 
+def parse(html):
+    info = list()
+    with open(html, 'r') as raw:
+        for line in raw:
+            if line.startswith('<table'):
+                break
+        for line in raw:
+            if line.strip().startswith('<tr>'):
+                record = list()
+                continue
+            record.append(line.strip())
+            if line.strip().startswith('</tr>'):
+                info.append(record)
+                continue
 
-info = list()
-with open(argv[1], 'r') as raw:
-    for line in raw:
-        if line.startswith('<table'):
-            break
-    for line in raw:
-        if line.strip().startswith('<tr>'):
-            record = list()
-            continue
-        record.append(line.strip())
-        if line.strip().startswith('</tr>'):
-            info.append(record)
-            continue
 
+def main():
+    url = 'http://www.onekp.com/public_data.html'
+    html = 'public.html'
+
+    if not os.path.exists(html):
+        down(url, html)
+    parse(html)
+    print('Done')
+
+
+if __name__ == '__main__':
+    main()
