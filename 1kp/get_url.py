5da6513bcf0a0c0269e6d454ef2b5db85cddf2b4
Workstation <wpwupingwp@outlook.com>
2017-08-02 17:36:12 +0800

    naive version works

--- 1kp/get_url.py
+++ 1kp/get_url.py
@@ -6,26 +6,33 @@ from urllib import request
 
 def down(url, html):
     raw = request.urlopen(url)
-    if raw.code != '200':
+    if raw.code != 200:
         raise Exception('Cannot open {}. Quit now.'.format(url))
     with open(html, 'w') as output:
-        output.write(raw.read())
+        output.write(raw.read().decode('utf-8'))
 
 
 def parse(html):
     info = list()
-    with open(html, 'r') as raw:
-        for line in raw:
-            if line.startswith('<table'):
-                break
-        for line in raw:
-            if line.strip().startswith('<tr>'):
-                record = list()
-                continue
-            record.append(line.strip())
-            if line.strip().startswith('</tr>'):
-                info.append(record)
-                continue
+    record = list()
+    handle = open(html, 'r')
+    for line in handle:
+        if line.startswith('<table'):
+            break
+    for line in handle:
+        if line.strip().startswith('<tr>'):
+            # record.clear()
+            record = list()
+            continue
+        record.append(line.strip())
+        if line.strip().startswith('</tr>'):
+            info.append(record)
+            continue
+        if line.strip().startswith('</table>'):
+            break
+
+    handle.close()
+    print(info)
 
 
 def main():
