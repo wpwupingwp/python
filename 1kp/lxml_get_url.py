74b21c52bdf1d3aaebe4f3adabd373f65efee1de
Workstation <wpwupingwp@outlook.com>
2017-08-02 19:24:42 +0800

    use space to separate href and text_content

--- 1kp/lxml_get_url.py
+++ 1kp/lxml_get_url.py
@@ -7,25 +7,29 @@ from lxml.html import parse
 def main():
     url = 'http://www.onekp.com/public_data.html'
     html = 'public.html'
+    handle = open('result.tsv', 'w')
 
     if not exists(html):
         raw = parse(url)
     else:
         raw = parse(html)
     # /html/body/table/tr/td/self|b|a.href
-    table = raw.find('/body/table')
-    for tr in table:
-        td = tr.findall('td')
-        for i in td:
+    all_tr = raw.xpath('//tr')
+    for tr in all_tr:
+        all_td = tr.findall('td')
+        for td in all_td:
             # some <td> have descdents, get a.href
-            for j in i.iter():
-                if j.tag == 'a':
-                    print('a', j.attrib['href'])
-                elif j.tag == 'td':
-                    print('td', j.text_content())
-            print()
-        print('-'*80)
-
+            for i in td.iter():
+                if i.tag == 'a':
+                    handle.write(i.attrib['href'])
+                    break
+                elif i.tag == 'td':
+                    handle.write(i.text_content()+' ')
+                else:
+                    pass
+            handle.write('\t')
+        handle.write('\n')
+    handle.close()
     print('Done')
 
 
