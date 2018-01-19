a56ba41e091901226e9f15d58757e1455a67d087
Workstation <wpwupingwp@outlook.com>
2018-01-19 12:38:51 +0800

    use pathlib

---
+++ rename_pathlib.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+from pathlib import Path
+import re
+
+# remove string
+pattern = re.compile(r'[\w]{5,}-')
+# use **/* to find all gz
+a = Path('.').glob('**/*.gz')
+# add id to solve repeat filename
+for i, j in enumerate(a):
+    # if in current dir
+    if j.parents[0] == Path('.'):
+        continue
+    # skip rawdata
+    if 'Rawdata' in j.parts:
+        print('Skip {}'.format(j))
+        continue
+    new = re.sub(pattern, '', j.name)
+    new_uniq = '{}-{}'.format(i, new)
+    # mv
+    j.rename(Path(j.parts[0]).joinpath(new_uniq))
