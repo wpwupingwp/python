cec2e70ccf809896a49a655d6f855520fa34bda1
Workstation <wpwupingwp@outlook.com>
2018-05-11 18:28:50 +0800

    use comma as sep

--- fuzzy_match.py
+++ fuzzy_match.py
@@ -6,9 +6,10 @@ from sys import argv
 tag = 'GTAGACTGCGTACC'
 max_mismatch = 3
 pattern = re.compile(r'({}){{e<={}}}'.format(tag, max_mismatch))
+print('Seq,Mismatch,Start,End')
 with open(argv[1], 'r') as raw:
     for line in raw:
         for match in re.finditer(pattern, line):
-            mismatch = sum(match.fuzzy_counts())
+            mismatch = sum(match.fuzzy_counts)
             loc = match.span()
-            print(line[:-1], mismatch, loc)
+            print(line[:-1], mismatch, *loc, sep=',')
