2ffe801e4dd613181f6547f9a4f6a42337fb9f3f
Workstation <wpwupingwp@outlook.com>
2018-05-11 18:16:58 +0800

    add fuzzy_match

---
+++ fuzzy_match.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import regex as re
+from sys import argv
+
+tag = 'GTAGACTGCGTACC'
+max_mismatch = 3
+pattern = re.compile(r'({}){{e<={}}}'.format(tag, max_mismatch))
+with open(argv[1], 'r') as raw:
+    for line in raw:
+        for match in re.finditer(pattern, line):
+            mismatch = sum(match.fuzzy_counts())
+            loc = match.span()
+            print(line[:-1], mismatch, loc)
