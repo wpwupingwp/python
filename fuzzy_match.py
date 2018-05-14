f0507ada822fec49ea2a991e72875ee7b9b31df5
Workstation <wpwupingwp@outlook.com>
2018-05-14 22:13:53 +0800

    add pattern2

--- fuzzy_match.py
+++ fuzzy_match.py
@@ -6,10 +6,20 @@ from sys import argv
 tag = 'GTAGACTGCGTACC'
 max_mismatch = 3
 pattern = re.compile(r'({}){{e<={}}}'.format(tag, max_mismatch))
-print('Seq,Mismatch,Start,End')
+pattern2 = re.compile(r'^(\w{5})\1')
+print('Seq,GoodBarcode,Mismatch,Start,End')
 with open(argv[1], 'r') as raw:
     for line in raw:
-        for match in re.finditer(pattern, line):
+        barcode = re.search(pattern2, line)
+        if barcode is None:
+            good_barcode = False
+        else:
+            good_barcode = True
+        match = re.search(pattern, line)
+        if match is None:
+            mismatch = ''
+            loc = ('', '')
+        else:
             mismatch = sum(match.fuzzy_counts)
             loc = match.span()
-            print(line[:-1], mismatch, *loc, sep=',')
+        print(line[:-1], str(good_barcode), mismatch, *loc, sep=',')
