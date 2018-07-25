880d94b277114c3307a97c34231d7609dfda602c
Workstation <wpwupingwp@outlook.com>
2018-07-25 20:36:09 +0800

    fasta or fastq

--- trim.py
+++ trim.py
@@ -8,16 +8,23 @@ def main():
     start = timer()
     print('Usage: python3 trim.py filename "from:to"')
     filename, from_to = argv[1:3]
+    with open(filename, 'r') as _:
+        line = _.readline()
+        if line.startswith('>'):
+            file_format = 'fasta'
+        else:
+            file_format = 'fastq'
+
     head, tail = from_to.split(':')
     head = int(head)
     if head > 0:
         head -= 1
     tail = int(tail)
     print(head, tail)
-    with open('{}.new.fasta'.format(filename), 'w') as output:
-        for record in SeqIO.parse(filename, 'fasta'):
+    with open('{}.new'.format(filename), 'w') as output:
+        for record in SeqIO.parse(filename, file_format):
             new = record[0:head] + record[tail:]
-            SeqIO.write(new, output, 'fasta')
+            SeqIO.write(new, output, file_format)
     end = timer()
     print('Cost {:.3f} seconds.'.format(end-start))
 
