e058f0464ed095de2566c916180b735434112579
Workstation <wpwupingwp@outlook.com>
2018-11-21 18:57:18 +0800

    use AlignIO and re.sub

---
+++ nex_for_mb.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+from sys import argv
+import re
+from Bio import AlignIO
+from Bio.Alphabet import IUPAC
+
+
+raw = AlignIO.read(argv[1], 'fasta', alphabet=IUPAC.ambiguous_dna)
+pattern = re.compile(r'\W')
+for i in raw:
+    i.id = re.sub(pattern, '_', i.id)
+    if len(i.id) >= 90:
+        print('{} -> ID longer than 90, cut!'.format(i.id))
+        i.id = i.id[:87] + '...'
+AlignIO.write(raw, argv[1]+'.nex', 'nexus')#, alphabet=IUPAC.ambiguous_dna)
