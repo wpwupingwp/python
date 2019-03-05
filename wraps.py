7db5e5cef40ec0d82dda06420ba691978399e63a
Workstation <wpwupingwp@outlook.com>
2019-03-05 20:18:47 +0800

    move wraps to wraps.py

---
+++ wraps.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+from functools import wraps
+from timeit import default_timer as timer
+
+
+def print_time(function):
+    @wraps(function)
+    def wrapper(*args, **kargs):
+        start = timer()
+        result = function(*args, **kargs)
+        end = timer()
+        print('The function {0} Cost {1:3f}s.\n'.format(
+            function.__name__, end-start))
+        return result
+    return wrapper
+
+
+@print_time
+def main():
+    print('test')
+
+
+if __name__ == '__main__':
+    main()
