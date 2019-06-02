a5051c86b8287942b59c2c9db50470959cc0f0c0
Workstation <wpwupingwp@outlook.com>
2019-06-02 18:39:07 +0800

    liuyanlei de join_fastq

--- join_fastq.py
+++ join_fastq.py
@@ -1,24 +1,20 @@
-﻿#!/usr/bin/python3
+#!usr/python3
 
+from sys import argv
 
-from Bio import SeqIO
-import sys
-
-left = SeqIO.parse(sys.argv[1], 'fastq')
-right = SeqIO.parse(sys.argv[2], 'fastq')
-handle = open('combine.fastq', 'w')
-offset = 32
-for l, r in zip(left, right):
-    l_seq = str(l.seq)
-    r_seq = str(r.seq.reverse_complement())
-    l_qual = ''.join([chr(i+offset) for i in l.letter_annotations[
-        'phred_quality']])
-    r_qual = ''.join([chr(i+offset) for i in r.letter_annotations[
-        'phred_quality']])
-    length = 500 - len(l_seq) - len(r_seq)
-    sequence = '{}{}{}'.format(l_seq, 'N'*length, r_seq)
-    quality = '{}{}{}'.format(l_qual, 'A'*length, r_qual[::-1])
-    name = l.description
-    handle.write('@{0}\n{1}\n+\n{2}\n'.format(name, sequence, quality))
-
-print('Done.')
+assert len(argv) == 4, (
+    'Usage: python3 join_fastq.py left right output')
+left = open(argv[1], 'r')
+right = open(argv[2], 'r')
+merge = open(argv[3], 'w')
+for a, b in zip(left, right):
+    merge.write(a)
+    a = left.readline()
+    b = right.readline()
+    merge.write(f'{a.strip()}{b}')
+    a = left.readline()
+    b = right.readline()
+    merge.write(a)
+    a = left.readline()
+    b = right.readline()
+    merge.write(f'{a.strip()}{b}')
