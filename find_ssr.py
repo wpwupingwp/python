5cf46b1845e19db1747cbe7708466e61a80b014a
Workstation <wpwupingwp@outlook.com>
2019-09-10 11:04:03 +0800

    fix ssr

--- find_ssr.py
+++ find_ssr.py
@@ -6,19 +6,21 @@ from timeit import default_timer as timer
 import re
 
 start = timer()
-pattern = re.compile(r'([ATCG]{1,6})\1{3,}')
+pattern = re.compile(r'([ATCG][ATCG]?[ATCG]?[ATCG]?[ATCG]?[ATCG]?)\1{2,}')
 found = 0
 with open(argv[1], 'r') as raw, open(argv[1]+'.ssr', 'w') as out:
-    out.write('id,sequence,unit,times\n')
+    out.write('id,unit,unit_len,times,start,sequence\n')
     for record in SeqIO.parse(raw, 'fasta'):
         # finditer can return group(0), findall cannot
         for i in re.finditer(pattern, str(record.seq)):
             found += 1
-            out.write('{},{},{},{}\n'.format(
+            out.write('{},{},{},{},{},{}\n'.format(
                 record.id,
-                i.group(0),
                 i.group(1),
-                len(i.group(0))//len(i.group(1))))
+                len(i.group(1)),
+                len(i.group(0))//len(i.group(1)),
+                i.start(),
+                i.group(0)))
 end = timer()
 print('Cost {:.2f} seconds.'.format(end-start))
 print('Found {} SSRs.'.format(found))
