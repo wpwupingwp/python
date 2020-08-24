a6910c96b31088ec6772ed4a8c2066bf9e317d16
Workstation <wpwupingwp@outlook.com>
2020-08-24 17:28:27 +0800

    asyncio do not support file IO

--- amerge_folder.py
+++ amerge_folder.py
@@ -19,22 +19,21 @@ async def main():
     """
     Try to use asyncio.
     """
-    print('python3 merge_folder.py source dest')
+    print('python3 amerge_folder.py source dest')
     source_dir = Path(argv[1]).absolute()
     dest_dir = Path(argv[2]).absolute()
-    sources = set(i.name for i in source_dir.glob('gene*'))
-    dests = set(i.name for i in dest_dir.glob('gene*'))
+    sources = set(i.name for i in source_dir.glob('*'))
+    dests = set(i.name for i in dest_dir.glob('*'))
     to_merge = dests.intersection(sources)
-    to_move = dests.difference(sources)
-    for i in to_merge:
-        source = source_dir / i
-        dest = dest_dir / i
-        asyncio.create_task(merge_file(source, dest))
-    # asyncio.wait(tasks)
+    to_move = sources.difference(dests)
     for j in to_move:
         source = source_dir / j
         dest = dest_dir / j
         move(source, dest)
+    for i in to_merge:
+        source = source_dir / i
+        dest = dest_dir / i
+        asyncio.create_task(merge_file(source, dest))
 
 
 asyncio.run(main())
