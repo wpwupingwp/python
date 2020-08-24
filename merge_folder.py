32611e68996d6cf4a9f9888c87b098b2ea3ae98e
Ping Wu <wpwupingwp@outlook.com>
2020-08-24 12:41:48 +0800

    create_task

--- merge_folder.py
+++ merge_folder.py
@@ -3,7 +3,6 @@
 from pathlib import Path
 from shutil import move
 from sys import argv
-from timeit import default_timer as timer
 import asyncio
 
 
@@ -16,11 +15,10 @@ async def merge_file(source, dest):
         await write_to(s, d)
 
 
-def main():
+async def main():
     """
     Try to use asyncio.
     """
-    start = timer()
     print('python3 merge_folder.py source dest')
     source_dir = Path(argv[1]).absolute()
     dest_dir = Path(argv[2]).absolute()
@@ -28,17 +26,15 @@ def main():
     dests = set(i.name for i in dest_dir.glob('gene*'))
     to_merge = dests.intersection(sources)
     to_move = dests.difference(sources)
-    loop = asyncio.get_event_loop()
     for i in to_merge:
         source = source_dir / i
         dest = dest_dir / i
-        loop.run_until_complete(merge_file(source, dest))
+        asyncio.create_task(merge_file(source, dest))
+    # asyncio.wait(tasks)
     for j in to_move:
         source = source_dir / j
         dest = dest_dir / j
         move(source, dest)
-    end = timer()
-    print(end-start, 'seconds')
 
 
-main()
+asyncio.run(main())
