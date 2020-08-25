e53ea40b470e4f409df0b7d77836a53012ec2d1c
Workstation <wpwupingwp@outlook.com>
2020-08-25 16:22:16 +0800

    plann only accepts ".gb"

--- split_ref.py
+++ split_ref.py
@@ -6,5 +6,5 @@ from sys import argv
 
 print('python3 split_ref.py file format')
 for index, record in enumerate(SeqIO.parse(argv[1], argv[2])):
-    out = f'{argv[1]}.{index}'
+    out = f'{index}-{argv[1]}'
     SeqIO.write(record, out, argv[2])
