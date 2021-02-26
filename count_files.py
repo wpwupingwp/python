a15cde71e7edd0bc01867d03dfdfbee00775cdce
Workstation <wpwupingwp@outlook.com>
2021-02-26 11:02:49 +0800

    count file/dir

--- count_files.py
+++ count_files.py
@@ -2,4 +2,18 @@ from sys import argv
 from pathlib import Path
 
 files = Path(argv[1]).glob('*')
-print(len(list(files))
+n_dir = 0
+n_file = 0
+n_other = 0
+
+for f in files:
+    if f.is_dir():
+        n_dir += 1
+    elif f.is_file():
+        n_file += 1
+    else:
+        n_other += 1
+print(n_dir, 'folders')
+print(n_file, 'files')
+print(n_other, 'other objects')
+print(n_dir+n_file+n_other, 'in all.')
