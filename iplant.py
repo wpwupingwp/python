943a0586eb8d5a68f91305d6ed665e8df5775957
Ping Wu <wpwupingwp@outlook.com>
2021-03-16 10:44:31 +0800

    rewrite iplant

--- iplant.py
+++ iplant.py
@@ -5,20 +5,60 @@ from urllib.parse import quote
 from urllib.request import urlopen
 from sys import argv
 from time import sleep
+try:
+    from lxml import etree
+except ImportError:
+    raise SystemExit('Cannot find lxml. Run "pip install --user lxml"')
 
 
 url = r'http://www.iplant.cn/info/'
-pattern = re.compile(r'<title>(?P<name>.*)</title>', re.DOTALL)
+# pattern = re.compile(r'<title>(?P<name>.*)</title>', re.DOTALL)
+pattern = re.compile(r'[A-Za-z ]+')
 
-
-def request(name):
-    sleep(1)
+def request(name: str) ->str:
+    sleep(0.5)
     fullurl = url + quote(name)
     request = urlopen(fullurl)
     if request.status != 200:
-        return name, ''
-    raw = request.read()
-    text = raw.decode('utf-8')
+        return ''
+    else:
+        raw = request.read()
+        text = raw.decode('utf-8')
+        return text
+
+
+def get_text(element: 'Element') ->str:
+    if len(element) == 0:
+        return ''
+    else:
+        text = element[0].text
+        if text is None:
+            text = ''
+        return text
+
+
+def parse(text: str):
+    #a = html.document_fromstring(raw)
+    a = etree.HTML(text)
+    cname_path = a.xpath('//*[@class="infocname"]')
+    cname = get_text(cname_path)
+    name_path = a.xpath('//*[@id="sptitlel"]')
+    name = get_text(name_path)
+    change_path = a.xpath('//*[@class="infomore"]/*[@class="spantxt"]/a')
+    change_name_raw = get_text(change_path)
+    _ = re.search(pattern, change_name_raw)
+    if _ is None:
+        change_name = ''
+    else:
+        change_name = _.group()
+    print(cname, name, change_name)
+    return cname, name, change_name
+
+
+def old():
+    name = ''
+    text = ''
+    title = ''
     search = re.search(pattern, text)
     if search.group() is not None:
         title = search.groupdict()['name'].strip()
@@ -34,13 +74,15 @@ def request(name):
 def main():
     raw = open(argv[1], 'r', encoding='utf-8')
     out = open(argv[1]+'-out.txt', 'w', encoding='utf-8')
-    miss = open(argv[1]+'-not_found.txt', 'w', encoding='utf-8')
+    out.write('Raw_name,Name,Chinese,Changed_Name\n')
     for line in raw:
-        name = line.strip()
-        chinese, latin = request(name)
-        if latin == '':
-            miss.write(f'{chinese}\n')
-        out.write(f'{chinese}#{latin}\n')
+        raw_name = line.strip()
+        text = request(raw_name)
+        if text == '':
+            cname, name, change_name = 'NOT_FOUND', raw_name, 'NOT_FOUND'
+        else:
+            cname, name, change_name = parse(text)
+        out.write(f'{raw_name},{name},{cname},{change_name}\n')
 
 
 if __name__ == '__main__':
