edcb9699f254a7ddd67eec1d3f8a052dd7e5ed29
Ping Wu <wpwupingwp@outlook.com>
2021-12-11 21:28:58 +0800

    seems work

--- keep_connect.py
+++ keep_connect.py
@@ -4,12 +4,14 @@ import requests
 import time
 import logging
 
-log = logging.getLogger()
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger('main')
 
 while True:
-    a = requests.get('https://www.ibcas.ac.cn')
-    log(f'{a.status_code} {a.url}')
-    time.sleep(30)
-    b = requests.get('https://www.baidu.com')
-    log(f'{b.status_code} {b.url}')
-    time.sleep(30)
+    a = requests.get('http://www.ibcas.ac.cn')
+    log.info(f'{a.status_code} {a.url}')
+    time.sleep(60)
+    b = requests.get('https://www.baidu.com', verify=True)
+    log.info(f'{b.status_code} {b.url}')
+    a = input('pause')
+    time.sleep(60)
