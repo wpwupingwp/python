41f2d1be81d8f22691a50e43fc37248d0f4a6f1b
Ping Wu <wpwupingwp@outlook.com>
2021-12-16 11:43:12 +0800

    move

--- keep_connect.py
+++ keep_connect.py
@@ -13,5 +13,5 @@ while True:
     time.sleep(60)
     b = requests.get('https://www.baidu.com', verify=True)
     log.info(f'{b.status_code} {b.url}')
-    a = input('pause')
+    # a = input('pause')
     time.sleep(60)
