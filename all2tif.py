96344ea9ed228866dc06d13c0ff57e7ab7ba38b2
Ping Wu <wuping@sicnu.edu.cn>
2022-11-07 17:16:37 +0800

    fix typo

--- all2tif.py
+++ all2tif.py
@@ -3,7 +3,7 @@ from pathlib import Path
 
 files = Path().glob('*.*')
 for f in files:
-    if f.suffix == '.tif'
+    if f.suffix == '.tif':
         continue
     with Image.open(f) as x:
         new = f.with_suffix('.tif')
