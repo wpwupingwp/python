d2e8be228a20291944dc72a08d220c52b614c836
Ping Wu <wuping@sicnu.edu.cn>
2022-11-07 17:15:53 +0800

    convert image type

---
+++ all2tif.py
@@ -0,0 +1,11 @@
+from PIL import Image
+from pathlib import Path
+
+files = Path().glob('*.*')
+for f in files:
+    if f.suffix == '.tif'
+        continue
+    with Image.open(f) as x:
+        new = f.with_suffix('.tif')
+        x.save(new, 'TIFF')
+        print(f, '->', new)
