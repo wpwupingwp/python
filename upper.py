f23d5bc52e31257d10057ca8220912f1a0e0e53d
Ping Wu <wuping@sicnu.edu.cn>
2022-11-08 14:53:22 +0800

    use string upper

--- upper.py
+++ upper.py
@@ -1,10 +1,24 @@
 #!/usr/bin/python3
-
-from Bio import SeqIO
 from sys import argv
 
-new = list()
-for record in SeqIO.parse(argv[1], 'fasta'):
-    record.seq = record.seq.upper()
-    new.append(record)
-SeqIO.write(new, argv[1]+'.new', 'fasta')
+
+def old():
+    from Bio import SeqIO
+
+    new = list()
+    for record in SeqIO.parse(argv[1], 'fasta'):
+        record.seq = record.seq.upper()
+        new.append(record)
+    SeqIO.write(new, argv[1]+'.new', 'fasta')
+
+
+def new():
+    out = argv[1]+'.new'
+    with open(old, 'r') as a, open(new, 'w') as b:
+        for line in a:
+            if not line.startswith('>'):
+                line = line.upper()
+            b.write(line)
+
+
+new()
