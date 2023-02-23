ae405239151199203f6741d9472cba45aaf6d62b
Ping Wu <wpwupingwp@outlook.com>
2023-02-23 22:32:21 +0800

    use powershell to run send pdf

---
+++ pdf/lairizhige.ps1
@@ -0,0 +1,9 @@
+$email="example@example.com"
+echo ${email}
+e:
+cd E:\Linux\python\venv\Scripts
+& .\Activate.ps1
+
+&python e:\linux\python\pdf\run.py e:\Onedrive\Sheet\扒谱\风吹鸡蛋壳.pdf ${email}  e:\linux\python\pdf\email.key
+c:
+pause
