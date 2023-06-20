9bac44d3910a2adf5b0913fafa4a0f74594dbcb8
Ping Wu <wpwupingwp@outlook.com>
2023-06-20 14:46:04 +0800

    excel set operation

---
+++ a_jian_b.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from sys import argv
+from pathlib import Path
+
+filename = Path(argv[1])
+
+data = pd.read_excel(filename)
+a = data.iloc[:, 0]
+b = data.iloc[:, 1]
+a_jian_b = set(a.unique()) - set(b.unique())
+out = pd.DataFrame(a_jian_b)
+out.to_excel(filename.with_name('chaji.xlsx'))
